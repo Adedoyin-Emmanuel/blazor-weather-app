76e63d4175fb3c603e709e9b1a53d3950c038718
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-09-22 06:59:40 +0100

    feat: added tailwindcss output file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,razor}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
