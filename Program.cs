a31c2f8e006b263f5069d07a83aa7ed4408c3b9d
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-09-22 06:36:21 +0100

    feat: initial commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
